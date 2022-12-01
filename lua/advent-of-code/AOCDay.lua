3a984537eb8553077e79c280b697d7e39c533430
Michael Strobel <mstrobel97@gmail.com>
2022-12-01 16:18:59 +0100

    🥚 update AOCDay to use setmetatable

--- lua/advent-of-code/AOCDay.lua
+++ lua/advent-of-code/AOCDay.lua
@@ -3,54 +3,52 @@
 ---@field two string|integer solution of second problem
 
 ---@class AOCDay
+---@field year string
+---@field day string
 ---@field lines table<integer, string>
 ---@field parse_input (fun(self: AOCDay): table) parses input
----@field read_input (fun(self: AOCDay, use_test_data: boolean): table) reads input
----@field solve (fun(self: AOCDay, use_test_data: boolean): Solution) solves all problems
 ---@field solve1 (fun(self: AOCDay): string|integer) solves first problem
 ---@field solve2 (fun(self: AOCDay): string|integer) solves second problem
-
----@param year string
----@param day string
----@return AOCDay
-local function create_object(year, day)
-  local obj = {
-    lines = {},
-  }
-
-  function obj:read_input(use_test_data)
+---@field solve (fun(self: AOCDay, use_test_data: boolean): Solution) solves all problems
+---@field new (fun(self: AOCDay, year: string, day: string): AOCDay) create new AOCDay
+local AOCDay = {
+  year = "",
+  day = "",
+  lines = {},
+  parse_input = function(self)
+    return self.lines
+  end,
+  solve1 = function(_)
+    return "problem 1 not solved yet"
+  end,
+  solve2 = function(_)
+    return "problem 2 not solved yet"
+  end,
+  solve = function(self, use_test_data)
     local file_name = use_test_data and "test.txt" or "input.txt"
-    local path = ("./lua/advent-of-code/%s/%s/%s"):format(year, day, file_name)
+    local path = ("./lua/advent-of-code/%s/%s/%s"):format(self.year, self.day, file_name)
     local file = io.open(path, "r")
 
     if file then
-      obj.lines = {}
+      self.lines = {}
       for line in file:lines() do
-        table.insert(obj.lines, line)
+        table.insert(self.lines, line)
       end
     end
-  end
-
-  function obj:solve1()
-    return "solve1 was not overwritten"
-  end
-
-  function obj:solve2()
-    return "solve2 was not overwritten"
-  end
-
-  function obj:parse_input() end
-
-  function obj:solve(use_test_data)
-    self:read_input(use_test_data)
 
     return {
       one = self:solve1(),
       two = self:solve2(),
     }
-  end
+  end,
+  new = function(self, year, day)
+    return setmetatable({
+      year = year,
+      day = day,
+    }, {
+      __index = self,
+    })
+  end,
+}
 
-  return obj
-end
-
-return create_object
+return AOCDay
