2b1f1b7c3908315f8e1340f881220cfad55c6407
Michael Strobel <mstrobel97@gmail.com>
2022-12-01 11:23:32 +0100

    🫁 reload package on every source

--- lua/advent-of-code/init.lua
+++ lua/advent-of-code/init.lua
@@ -1,11 +1,12 @@
-local package = ("advent-of-code.%s.%s"):format(os.date "%Y", os.date "%d")
-local ok = pcall(require, package)
+local module = ("advent-of-code.%s.%s"):format(os.date "%Y", os.date "%d")
+package.loaded[module] = nil
+local ok = pcall(require, module)
 
 if not ok then
   return
 end
 
 ---@type AOCDay
-local day = require(package)
+local day = require(module)
 
 vim.pretty_print(day:solve(true))
