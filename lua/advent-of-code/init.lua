75dfc344fa872217395c7e50a0fdaac6926634b5
Michael Strobel <mstrobel97@gmail.com>
2022-12-06 13:06:26 +0100

    feat(base): update main file/new_day
    now also works with custom year and day
    the auto generated commit now also mentions the year

--- lua/advent-of-code/init.lua
+++ lua/advent-of-code/init.lua
@@ -1,14 +1,22 @@
-local module = ("advent-of-code.%s.%s"):format(os.date "%Y", os.date "%d")
-package.loaded[module] = nil
-local ok = pcall(require, module)
+local function main(use_default_input, year, day)
+  use_default_input = use_default_input == nil and true or use_default_input
+  year = year or os.date "%Y"
+  day = day or os.date "%d"
 
-if not ok then
-  os.execute "make"
+  local module = ("advent-of-code.%s.%s"):format(year, day)
+  package.loaded[module] = nil
+  local ok = pcall(require, module)
+
+  if not ok then
+    os.execute(("./new_day -D %s:%s"):format(year, day))
+  end
+
+  ---@type AOCDay
+  local aocday = require(module)
+
+  local solution = aocday:solve(use_default_input)
+  print("solution one: " .. solution.one)
+  print("solution two: " .. solution.two)
 end
 
----@type AOCDay
-local day = require(module)
-
-local solution = day:solve(true)
-print("solution one: " .. solution.one)
-print("solution two: " .. solution.two)
+main()
