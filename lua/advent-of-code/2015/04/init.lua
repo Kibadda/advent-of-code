b523f28cf2a8fcc2bb8421f8beb5dbd2bc57e6be
Michael Strobel <mstrobel97@gmail.com>
2022-12-07 14:58:59 +0100

    feat(2015/04): add solution

--- lua/advent-of-code/2015/04/init.lua
+++ lua/advent-of-code/2015/04/init.lua
@@ -1,5 +1,33 @@
 local AOCDay = require "advent-of-code.AOCDay"
 
+local md5 = require "advent-of-code.2015.04.md5"
+
 local M = AOCDay:new("2015", "04")
+
+function M:solve1()
+  local secret = self.lines[1]
+  local hash
+
+  local i = -1
+  repeat
+    i = i + 1
+    hash = md5.sumhexa(secret .. i)
+  until hash:sub(1, 5) == "00000"
+
+  return i
+end
+
+function M:solve2()
+  local secret = self.lines[1]
+  local hash
+
+  local i = -1
+  repeat
+    i = i + 1
+    hash = md5.sumhexa(secret .. i)
+  until hash:sub(1, 6) == "000000"
+
+  return i
+end
 
 return M
