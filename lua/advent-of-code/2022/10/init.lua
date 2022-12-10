2382176f2d91ff7012a9e7110e70f9f15c243c76
Michael Strobel <mstrobel97@gmail.com>
2022-12-10 08:37:23 +0100

    feat(2022/10): add solution

---
+++ lua/advent-of-code/2022/10/init.lua
@@ -0,0 +1,88 @@
+local AOCDay = require "advent-of-code.AOCDay"
+
+local M = AOCDay:new("2022", "10")
+
+function M:solve1()
+  local signal = 0
+
+  local x = 1
+  local cycle = 1
+  for _, line in ipairs(self.lines) do
+    local split = line:split()
+    if split[1] == "noop" then
+      cycle = cycle + 1
+      if (cycle + 20) % 40 == 0 then
+        signal = signal + x * cycle
+      end
+    else
+      for i = 1, 2 do
+        cycle = cycle + 1
+        if i == 2 then
+          x = x + split[2]
+        end
+        if (cycle + 20) % 40 == 0 then
+          signal = signal + x * cycle
+        end
+      end
+    end
+  end
+
+  return signal
+end
+
+function M:solve2()
+  local image = { ["1"] = "" }
+
+  local x = 1
+  local cycle = 1
+
+  local function check_pos()
+    local pos = x
+    if pos == 1 then
+      pos = 2
+    elseif pos == 40 then
+      pos = 39
+    end
+
+    local crt_pos = (cycle - 1) % 40
+    return crt_pos == pos or crt_pos == pos - 1 or crt_pos == pos + 1
+  end
+
+  local index = 1
+  for _, line in ipairs(self.lines) do
+    local split = line:split()
+    if split[1] == "noop" then
+      if check_pos() then
+        image["" .. index] = image["" .. index] .. "#"
+      else
+        image["" .. index] = image["" .. index] .. "."
+      end
+      cycle = cycle + 1
+      if cycle % 40 == 1 then
+        index = index + 1
+        image["" .. index] = ""
+      end
+    else
+      for i = 1, 2 do
+        if check_pos() then
+          image["" .. index] = image["" .. index] .. "#"
+        else
+          image["" .. index] = image["" .. index] .. "."
+        end
+        cycle = cycle + 1
+        if cycle % 40 == 1 then
+          index = index + 1
+          image["" .. index] = ""
+        end
+        if i == 2 then
+          x = x + split[2]
+        end
+      end
+    end
+  end
+
+  vim.pretty_print(image)
+  return 1
+end
+
+return M
