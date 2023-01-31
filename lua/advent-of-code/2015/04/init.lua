862c1985fa2c056b3c74de1f82eb220107d59961
Michael Strobel <mstrobel97@gmail.com>
2023-01-31 09:43:46 +0100

    feat(2015/04): update solution

--- lua/advent-of-code/2015/04/init.lua
+++ lua/advent-of-code/2015/04/init.lua
@@ -5,7 +5,7 @@ local md5 = require "advent-of-code.2015.04.md5"
 
 local M = AOC.create("2015", "04")
 
-function M:solve1()
+function M:solver(ending)
   local secret = self.input[1]
   local hash
 
@@ -13,22 +13,17 @@ function M:solve1()
   repeat
     i = i + 1
     hash = md5.sumhexa(secret .. i)
-  until hash:sub(1, 5) == "00000"
+  until hash:sub(1, 5) == ending
 
-  self.solution:add("1", i)
+  return i
+end
+
+function M:solve1()
+  self.solution:add("1", self:solver "00000")
 end
 
 function M:solve2()
-  local secret = self.input[1]
-  local hash
-
-  local i = -1
-  repeat
-    i = i + 1
-    hash = md5.sumhexa(secret .. i)
-  until hash:sub(1, 6) == "000000"
-
-  self.solution:add("2", i)
+  self.solution:add("2", self:solver "000000")
 end
 
 M:run(false)
