b055192e2a9353cf4b2e5ed3c5bdce9a884f754d
Michael Strobel <mstrobel97@gmail.com>
2023-03-02 13:39:52 +0100

    feat(base): update helper functions

--- lua/advent-of-code/helpers/Vector.lua
+++ lua/advent-of-code/helpers/Vector.lua
@@ -15,20 +15,51 @@ local mt = {
   __eq = function(v1, v2)
     return v1.x == v2.x and v1.y == v2.y
   end,
+  ---@param v Vector
+  ---@param n string|number
+  ---@return Vector
+  __mul = function(v, n)
+    if type(n) == "number" then
+      return V(v.x * n, v.y * n)
+    elseif type(n) == "string" then
+      if n == "L" then
+        return V(v.x == 0 and (v.y * -1) or v.y, v.y == 0 and v.x or (v.x * -1))
+      elseif n == "R" then
+        return V(v.x == 0 and v.y or (v.y * -1), v.y == 0 and (v.x * -1) or v.x)
+      end
+    end
+
+    return v
+  end,
   __index = Vector,
 }
 
 Vector = {
+  ---@param x number
+  ---@param y number
   new = function(x, y)
     return setmetatable({
       x = x,
       y = y,
     }, mt)
   end,
+  ---@param v1 Vector
+  ---@param v2 Vector
+  distance = function(v1, v2)
+    return math.abs(v1.x - v2.x) + math.abs(v1.y - v2.y)
+  end,
 }
 
+---@param x number
+---@param y number
 function _G.V(x, y)
   return Vector.new(x, y)
+end
+
+---@param v1 Vector
+---@param v2 Vector
+function _G.Vdistance(v1, v2)
+  return Vector.distance(v1, v2)
 end
 
 return Vector
