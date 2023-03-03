d6d3364d7e75144a0fd1208b2eb325f958d46400
Michael Strobel <mstrobel97@gmail.com>
2023-03-03 09:14:10 +0100

    📅 2016/05: add solution

---
+++ lua/advent-of-code/2016/05/init.lua
@@ -0,0 +1,50 @@
+local AOC = require "advent-of-code.AOC"
+AOC.reload()
+
+local md5 = require "advent-of-code.2015.04.md5"
+
+local M = AOC.create("2016", "05")
+
+function M:parse_input(file)
+  for line in file:lines() do
+    self.input = line
+  end
+end
+
+function M:solve1()
+  local i = -1
+  local pass = ""
+  for _ = 1, 8 do
+    local hash
+    repeat
+      i = i + 1
+      print(i)
+      hash = md5.sumhexa(self.input .. i)
+    until hash:sub(1, 5) == "00000"
+    pass = pass .. hash:at(6)
+  end
+
+  self.solution:add("1", pass)
+end
+
+function M:solve2()
+  local i = -1
+  local pass = {}
+  repeat
+    local hash
+    repeat
+      i = i + 1
+      hash = md5.sumhexa(self.input .. i)
+    until hash:sub(1, 5) == "00000"
+    local number = tonumber(hash:at(6))
+    if number and number >= 0 and number <= 7 and pass[number + 1] == nil then
+      pass[number + 1] = hash:at(7)
+    end
+  until table.count(pass) == 8
+
+  self.solution:add("2", table.concat(pass, ""))
+end
+
+M:run(false)
+
+return M
