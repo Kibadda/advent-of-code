8dc3cdbefc0c97c47e3ef2452f4498fc9955bcaa
Michael Strobel <mstrobel97@gmail.com>
2023-12-04 13:08:28 +0100

    refactor(base): use more table functions

--- lua/advent-of-code/2023/03/init.lua
+++ lua/advent-of-code/2023/03/init.lua
@@ -41,50 +41,52 @@ function M:parse_input(file)
 end
 
 function M:solve1()
-  local parts = 0
+  self.solution:add(
+    "1",
+    table.reduce(self.input.numbers, 0, function(parts, number)
+      local found = false
+      for i = number.line - 1, number.line + 1 do
+        for j = number.s - 1, number.e + 1 do
+          if self.input.grid[i] and j >= 1 and j <= #self.input.grid[i] then
+            if not tonumber(self.input.grid[i]:at(j)) and self.input.grid[i]:at(j) ~= "." then
+              parts = parts + number.num
+              found = true
+            end
+          end
+        end
 
-  for _, number in ipairs(self.input.numbers) do
-    local found = false
-    for i = number.line - 1, number.line + 1 do
-      for j = number.s - 1, number.e + 1 do
-        if self.input.grid[i] and j >= 1 and j <= #self.input.grid[i] then
-          if not tonumber(self.input.grid[i]:at(j)) and self.input.grid[i]:at(j) ~= "." then
-            parts = parts + number.num
-            found = true
+        if found then
+          break
+        end
+      end
+
+      return parts
+    end)
+  )
+end
+
+function M:solve2()
+  self.solution:add(
+    "2",
+    table.reduce(self.input.grid, 0, function(gears, line, i)
+      for j = 1, #line do
+        if line:at(j) == "*" then
+          local ad = table.filter(self.input.numbers, function(number)
+            return not (i + 1 < number.line)
+              and not (i - 1 > number.line)
+              and not (j + 1 < number.s)
+              and not (j - 1 > number.e)
+          end)
+
+          if #ad == 2 then
+            gears = gears + ad[1].num * ad[2].num
           end
         end
       end
 
-      if found then
-        break
-      end
-    end
-  end
-
-  self.solution:add("1", parts)
-end
-
-function M:solve2()
-  local gears = 0
-
-  for i, line in ipairs(self.input.grid) do
-    for j = 1, #line do
-      if line:at(j) == "*" then
-        local ad = table.filter(self.input.numbers, function(number)
-          return not (i + 1 < number.line)
-            and not (i - 1 > number.line)
-            and not (j + 1 < number.s)
-            and not (j - 1 > number.e)
-        end)
-
-        if #ad == 2 then
-          gears = gears + ad[1].num * ad[2].num
-        end
-      end
-    end
-  end
-
-  self.solution:add("2", gears)
+      return gears
+    end)
+  )
 end
 
 M:run()
