ad7eb72491316149efe9a596304a5d8a9d650cc6
Michael Strobel <mstrobel97@gmail.com>
2023-12-08 14:38:20 +0100

    feat(2015/03): update solution

--- lua/advent-of-code/2015/03/init.lua
+++ lua/advent-of-code/2015/03/init.lua
@@ -1,57 +1,58 @@
 local AOC = require "advent-of-code.AOC"
 AOC.reload()
 
+---@class AOCDay201503: AOCDay
+---@field input string
 local M = AOC.create("2015", "03")
 
-function M:solver(c, pos, grid)
-  if c == "^" then
-    pos[1] = pos[1] + 1
-  elseif c == "v" then
-    pos[1] = pos[1] - 1
-  elseif c == ">" then
-    pos[2] = pos[2] + 1
-  elseif c == "<" then
-    pos[2] = pos[2] - 1
+---@param file file*
+function M:parse(file)
+  self.input = file:read()
+end
+
+---@param sleighs integer
+function M:solver(sleighs)
+  ---@type Vector[]
+  local pos = {}
+  for _ = 1, sleighs do
+    table.insert(pos, V(0, 0))
   end
-  local x = tostring(pos[1])
-  local y = tostring(pos[2])
-  if grid[x] == nil then
-    grid[x] = {}
+
+  local iter = cycle(pos)
+  local index = 1
+
+  local grid = {}
+  local houses = 0
+
+  for _, c in ipairs(self.input:to_list()) do
+    if c == "^" then
+      pos[index] = pos[index] + V(1, 0)
+    elseif c == "v" then
+      pos[index] = pos[index] + V(-1, 0)
+    elseif c == ">" then
+      pos[index] = pos[index] + V(0, 1)
+    elseif c == "<" then
+      pos[index] = pos[index] + V(0, -1)
+    end
+
+    if not grid[pos[index]:string()] then
+      houses = houses + 1
+    end
+
+    grid[pos[index]:string()] = true
+
+    index = iter(pos, index)
   end
-  if grid[x][y] == nil then
-    grid[x][y] = true
-    return true
-  end
-  return false
+
+  return houses
 end
 
 function M:solve1()
-  local houses = 1
-  local pos = { 0, 0 }
-  local grid = { ["0"] = { ["0"] = true } }
-  for _, c in ipairs(self.input[1]:to_list()) do
-    if self:solver(c, pos, grid) then
-      houses = houses + 1
-    end
-  end
-
-  self.solution:add("1", houses)
+  return self:solver(1)
 end
 
 function M:solve2()
-  local houses = 1
-  local santa_pos = { 0, 0 }
-  local robo_pos = { 0, 0 }
-  local grid = { ["0"] = { ["0"] = true } }
-  local turn = "santa"
-  for _, c in ipairs(self.input[1]:to_list()) do
-    if self:solver(c, turn == "santa" and santa_pos or robo_pos, grid) then
-      houses = houses + 1
-    end
-    turn = turn == "santa" and "robo" or "santa"
-  end
-
-  self.solution:add("2", houses)
+  return self:solver(2)
 end
 
 M:run()
