9316e54578210d1134a3bcd167cd7ef7e12df4b4
Michael Strobel <mstrobel97@gmail.com>
2023-12-09 10:35:07 +0100

    feat(2023/09): add solution

---
+++ lua/advent-of-code/2023/09/init.lua
@@ -0,0 +1,59 @@
+local AOC = require "advent-of-code.AOC"
+AOC.reload()
+
+---@class AOCDay202309: AOCDay
+---@field input integer[][]
+local M = AOC.create("2023", "09")
+
+---@param file file*
+function M:parse(file)
+  for line in file:lines() do
+    table.insert(self.input, line:only_ints "-?%d+")
+  end
+end
+
+---@param reverse? boolean
+---@return integer
+function M:solver(reverse)
+  local total = 0
+
+  for _, numbers in ipairs(self.input) do
+    local sequences = reverse and { table.reverse(numbers) } or { numbers }
+    local index = 1
+
+    while
+      not table.reduce(sequences[index], true, function(is_zero, number)
+        return is_zero and (number == 0)
+      end)
+    do
+      local sequence = {}
+      for i = 1, #sequences[index] - 1 do
+        table.insert(sequence, sequences[index][i + 1] - sequences[index][i])
+      end
+
+      table.insert(sequences, sequence)
+      index = index + 1
+    end
+
+    local new_value = 0
+    for i = #sequences - 1, 1, -1 do
+      new_value = sequences[i][#sequences[i]] + new_value
+    end
+
+    total = total + new_value
+  end
+
+  return total
+end
+
+function M:solve1()
+  return self:solver()
+end
+
+function M:solve2()
+  return self:solver(true)
+end
+
+M:run()
+
+return M
