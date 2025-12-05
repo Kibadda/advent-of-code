3e32de65eea876c21ff8bd6c68fde977ddf699be
Michael Strobel <mstrobel97@gmail.com>
2025-12-05 15:24:35 +0100

    feat(2015/25): add solution

---
+++ lua/advent-of-code/2015/25/init.lua
@@ -0,0 +1,37 @@
+--- @class AOCDay201525: AOCDay
+--- @field input { row: number, col: number }
+local M = require("advent-of-code.AOC").create("2015", "25")
+
+--- @param file file*
+function M:parse(file)
+  local ints = file:lines()():only_ints()
+  self.input = { row = ints[1], col = ints[2] }
+end
+
+function M:solve1()
+  local number = 20151125
+  local row = 1
+  local col = 1
+
+  while true do
+    if row == 1 then
+      row = col + 1
+      col = 1
+    else
+      row = row - 1
+      col = col + 1
+    end
+
+    number = (number * 252533) % 33554393
+
+    if row == self.input.row and col == self.input.col then
+      break
+    end
+  end
+
+  return number
+end
+
+function M:solve2() end
+
+M:run()
