707acfc4a0b6a5337ad7e12c665b80e17ba346c2
Michael Strobel <mstrobel97@gmail.com>
2025-12-07 20:01:01 +0100

    📅 2025/07: add solution

---
+++ lua/advent-of-code/2025/07/init.lua
@@ -0,0 +1,62 @@
+--- @class AOCDay202507: AOCDay
+--- @field input string[][]
+local M = require("advent-of-code.AOC").create("2025", "07")
+
+--- @param file file*
+function M:parse(file)
+  for line in file:lines() do
+    table.insert(self.input, line:to_list())
+  end
+end
+
+function M:solve1()
+  local grid = table.deepcopy(self.input)
+
+  local split = 0
+  for i, row in ipairs(grid) do
+    for j, col in ipairs(row) do
+      if col == "S" or col == "|" then
+        if i + 1 <= #grid then
+          if grid[i + 1][j] == "^" then
+            split = split + 1
+            if j - 1 > 0 then
+              grid[i + 1][j - 1] = "|"
+            end
+            if j + 1 <= #row then
+              grid[i + 1][j + 1] = "|"
+            end
+          else
+            grid[i + 1][j] = "|"
+          end
+        end
+      end
+    end
+  end
+
+  return split
+end
+
+function M:solve2()
+  local beams = { [table.concat(self.input[1]):find "S"] = 1 }
+
+  for i = 1, #self.input - 1 do
+    local new_beams = {}
+    for beam, number in spairs(beams) do
+      if self.input[i + 1][beam] == "^" then
+        if beam - 1 >= 1 then
+          new_beams[beam - 1] = (new_beams[beam - 1] or 0) + number
+        end
+        if beam + 1 <= #self.input[i] then
+          new_beams[beam + 1] = (new_beams[beam + 1] or 0) + number
+        end
+      else
+        new_beams[beam] = (new_beams[beam] or 0) + number
+      end
+    end
+    beams = new_beams
+  end
+
+  return table.sum(beams, pairs)
+end
+
+M:run()
