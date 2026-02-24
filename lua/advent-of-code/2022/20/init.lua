ffdf25618b6243e7c8765b42c0fe1d4732f3770b
Michael Strobel <mstrobel97@gmail.com>
2026-02-24 14:54:15 +0100

    feat(2022/20): add solution

--- lua/advent-of-code/2022/20/init.lua
+++ lua/advent-of-code/2022/20/init.lua
@@ -4,63 +4,67 @@ local M = require("advent-of-code.AOCDay"):new("2022", "20")
 
 --- @param lines string[]
 function M:parse(lines)
-  for i, line in ipairs(lines) do
-    table.insert(self.input, ("%d|%d"):format(i, line))
+  for _, line in ipairs(lines) do
+    table.insert(self.input, tonumber(line))
   end
 end
 
-local function move(t, from, to)
-  if from < to then
-    table.insert(t, to + 1, t[from])
-    table.remove(t, from)
-  else
-    table.insert(t, to, t[from])
-    table.remove(t, from + 1)
-  end
-end
+function M:solver(multiplier, shuffle)
+  local original = table.map(self.input, function(n, i)
+    return string.format("%d|%d", i, n * multiplier)
+  end)
+  local numbers = table.deepcopy(original)
+  local count = table.count(numbers)
 
-function M:solve1()
-  local numbers = table.deepcopy(self.input)
-  for _, val in ipairs(self.input) do
-    local cur = table.find(numbers, val)
-    local num = tonumber(val:split("|")[2])
-    local new
-    if num < 0 then
-      if cur + num < 1 then
-        new = (cur + num - 1) % table.count(numbers)
-      else
-        new = cur + num
-      end
-    elseif num > 0 then
-      if cur + num > table.count(numbers) then
-        new = (cur + num + 1) % table.count(numbers)
-      else
-        new = cur + num
-      end
+  local function move(from, to)
+    if to == 1 then
+      to = count
     end
 
-    if new ~= nil then
-      move(numbers, cur, new)
+    if from < to then
+      table.insert(numbers, to + 1, numbers[from])
+      table.remove(numbers, from)
+    else
+      table.insert(numbers, to, numbers[from])
+      table.remove(numbers, from + 1)
     end
   end
 
-  local zero_index
+  for _ = 1, shuffle do
+    for _, val in ipairs(original) do
+      local cur = table.find(numbers, val)
+      local num = assert(tonumber(string.split(val, "|")[2]))
+      local new = cur + num
+
+      if new >= 1 and new <= count then
+        move(cur, new)
+      else
+        move(cur, ((new - 1) % (count - 1)) + 1)
+      end
+    end
+  end
+
+  local zero
   for i, val in ipairs(numbers) do
     if tonumber(val:split("|")[2]) == 0 then
-      zero_index = i
+      zero = i
       break
     end
   end
 
-  local one = numbers[(zero_index + 1000) % table.count(numbers)]:split("|")[2]
-  local two = numbers[(zero_index + 2000) % table.count(numbers)]:split("|")[2]
-  local three = numbers[(zero_index + 3000) % table.count(numbers)]:split("|")[2]
+  local one = numbers[((zero + 999) % count) + 1]:split("|")[2]
+  local two = numbers[((zero + 1999) % count) + 1]:split("|")[2]
+  local three = numbers[((zero + 2999) % count) + 1]:split("|")[2]
 
   return one + two + three
 end
 
+function M:solve1()
+  return self:solver(1, 1)
+end
+
 function M:solve2()
-  --
+  return self:solver(811589153, 10)
 end
 
 M:run()
