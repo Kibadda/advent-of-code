daf0a0ce170d51f0571d1c1f13558587e2f87640
Michael Strobel <mstrobel97@gmail.com>
2026-09-23 10:12:46 +0200

    chore(base): format flake.nix

--- flake.nix
+++ flake.nix
@@ -9,131 +9,133 @@
       system = "x86_64-linux";
     in
     {
-      devShells.${system}.default = let
-        pkgs = import nixpkgs { inherit system; };
-      in pkgs.mkShell {
-        name = "advent-of-code";
-        buildInputs = [
-          (pkgs.writeShellApplication {
-            name = "aoc";
-            runtimeInputs = with pkgs; [
-              aoc-cli
-              luajit
-              jq
-            ];
-            text = ''
-              wrapper() {
-                aoc -s ./aoc.session "$@"
-              }
+      devShells.${system}.default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          name = "advent-of-code";
+          buildInputs = [
+            (pkgs.writeShellApplication {
+              name = "aoc";
+              runtimeInputs = with pkgs; [
+                aoc-cli
+                luajit
+                jq
+              ];
+              text = ''
+                wrapper() {
+                  aoc -s ./aoc.session "$@"
+                }
 
-              if [ "$#" -eq 0 ]; then
-                wrapper help
-                exit 0
-              fi
+                if [ "$#" -eq 0 ]; then
+                  wrapper help
+                  exit 0
+                fi
 
-              case "$1" in
-                r)
-                  if [ "$#" -lt 3 ]; then
-                    exit 1
-                  fi
-                  test=0
-                  if [ "$#" -eq 4 ]; then
-                    test=$4
-                  fi
-                  if [[ ! -e "lua/advent-of-code/$2/$3/input.txt" ]]; then
-                    "$0" d "$2" "$3"
-                  fi
-                  cd ./lua
-                  luajit "advent-of-code/$2/$3/init.lua" "$test"
-                  ;;
-                s)
-                  if [ "$#" -lt 5 ]; then
-                    exit 1
-                  fi
-                  wrapper s -y "$2" -d "$3" "$4" "$5"
-                  ;;
-                d)
-                  if [ "$#" -lt 3 ]; then
-                    exit 1
-                  fi
-                  wrapper d -y "$2" -d "$3" -I -i "lua/advent-of-code/$2/$3/input.txt" -o
-                  ;;
-                n)
-                  if [ "$#" -lt 3 ]; then
-                    exit 1
-                  fi
-                  if [ "$(git branch --show-current)" != "main" ]; then
-                    echo "only create new days on main branch; exiting"
-                    exit 1
-                  fi
-                  BRANCH="feat/$2-$3"
-                  DIR="./lua/advent-of-code/$2/$3"
-                  if git branch --list "$BRANCH" | grep -q .; then
-                    echo "$BRANCH already exists; switching"
-                    git switch "$BRANCH"
-                  elif [[ -d "$DIR/" ]]; then
-                    echo "$DIR already exists; exiting"
-                  else
-                    mkdir -p "$DIR"
-                    wrapper d -I -i "$DIR/input.txt" -y "$2" -d "$3"
-                    touch "$DIR/test.txt"
-                    cp "lua/advent-of-code/day_template.lua" "$DIR/init.lua"
-                    sed -i "s/YEAR/$2/g; s/DAY/$3/g" "$DIR/init.lua"
-                    git switch -c "$BRANCH"
-                  fi
-                  ;;
-                t)
-                  if [ "$#" -lt 3 ]; then
-                    exit 1
-                  fi
-                  THRESHOLD_SEC="''${THRSHOLD_SEC:-1}"
-                  THRESHOLD_US=$(awk "BEGIN {print $THRESHOLD_SEC * 1000000}")
-                  sum_parse=0
-                  sum_one=0
-                  sum_two=0
-                  runtime=0
-                  count=0
-                  cd ./lua
-                  while true
-                  do
-                    count=$((count + 1))
-                    json="$(TIMINGS=1 luajit "advent-of-code/$2/$3/init.lua")"
-                    start=$(jq -r '.start' <<<"$json")
-                    parse=$(jq -r '.parse' <<<"$json")
-                    one=$(jq -r '.one' <<<"$json")
-                    two=$(jq -r '.two' <<<"$json")
-                    d_parse=$((parse - start))
-                    d_one=$((one - parse))
-                    d_two=$((two - one))
-                    d_all=$((two - start))
-                    sum_parse=$((sum_parse + d_parse))
-                    sum_one=$((sum_one + d_one))
-                    sum_two=$((sum_two + d_two))
-                    runtime=$((runtime + d_all))
-                    if (( runtime > THRESHOLD_US )); then
-                      break
+                case "$1" in
+                  r)
+                    if [ "$#" -lt 3 ]; then
+                      exit 1
                     fi
-                  done
-                  avg_parse=$((sum_parse))
-                  avg_one=$((sum_one))
-                  avg_two=$((sum_two))
-                  if (( count > 0 )); then
-                    avg_parse=$((sum_parse / count))
-                    avg_one=$((sum_one / count))
-                    avg_two=$((sum_two / count))
-                  fi
-                  echo "Averaged over $count runs:"
-                  echo "  parse:  $avg_parse"
-                  echo "  one:    $avg_one"
-                  echo "  two:    $avg_two"
-                  ;;
-                *)
-                  wrapper "$@"
-                  ;;
-              esac
-            '';
-          })
-        ];
-      };
-  };
+                    test=0
+                    if [ "$#" -eq 4 ]; then
+                      test=$4
+                    fi
+                    if [[ ! -e "lua/advent-of-code/$2/$3/input.txt" ]]; then
+                      "$0" d "$2" "$3"
+                    fi
+                    cd ./lua
+                    luajit "advent-of-code/$2/$3/init.lua" "$test"
+                    ;;
+                  s)
+                    if [ "$#" -lt 5 ]; then
+                      exit 1
+                    fi
+                    wrapper s -y "$2" -d "$3" "$4" "$5"
+                    ;;
+                  d)
+                    if [ "$#" -lt 3 ]; then
+                      exit 1
+                    fi
+                    wrapper d -y "$2" -d "$3" -I -i "lua/advent-of-code/$2/$3/input.txt" -o
+                    ;;
+                  n)
+                    if [ "$#" -lt 3 ]; then
+                      exit 1
+                    fi
+                    if [ "$(git branch --show-current)" != "main" ]; then
+                      echo "only create new days on main branch; exiting"
+                      exit 1
+                    fi
+                    BRANCH="feat/$2-$3"
+                    DIR="./lua/advent-of-code/$2/$3"
+                    if git branch --list "$BRANCH" | grep -q .; then
+                      echo "$BRANCH already exists; switching"
+                      git switch "$BRANCH"
+                    elif [[ -d "$DIR/" ]]; then
+                      echo "$DIR already exists; exiting"
+                    else
+                      mkdir -p "$DIR"
+                      wrapper d -I -i "$DIR/input.txt" -y "$2" -d "$3"
+                      touch "$DIR/test.txt"
+                      cp "lua/advent-of-code/day_template.lua" "$DIR/init.lua"
+                      sed -i "s/YEAR/$2/g; s/DAY/$3/g" "$DIR/init.lua"
+                      git switch -c "$BRANCH"
+                    fi
+                    ;;
+                  t)
+                    if [ "$#" -lt 3 ]; then
+                      exit 1
+                    fi
+                    THRESHOLD_SEC="''${THRSHOLD_SEC:-1}"
+                    THRESHOLD_US=$(awk "BEGIN {print $THRESHOLD_SEC * 1000000}")
+                    sum_parse=0
+                    sum_one=0
+                    sum_two=0
+                    runtime=0
+                    count=0
+                    cd ./lua
+                    while true
+                    do
+                      count=$((count + 1))
+                      json="$(TIMINGS=1 luajit "advent-of-code/$2/$3/init.lua")"
+                      start=$(jq -r '.start' <<<"$json")
+                      parse=$(jq -r '.parse' <<<"$json")
+                      one=$(jq -r '.one' <<<"$json")
+                      two=$(jq -r '.two' <<<"$json")
+                      d_parse=$((parse - start))
+                      d_one=$((one - parse))
+                      d_two=$((two - one))
+                      d_all=$((two - start))
+                      sum_parse=$((sum_parse + d_parse))
+                      sum_one=$((sum_one + d_one))
+                      sum_two=$((sum_two + d_two))
+                      runtime=$((runtime + d_all))
+                      if (( runtime > THRESHOLD_US )); then
+                        break
+                      fi
+                    done
+                    avg_parse=$((sum_parse))
+                    avg_one=$((sum_one))
+                    avg_two=$((sum_two))
+                    if (( count > 0 )); then
+                      avg_parse=$((sum_parse / count))
+                      avg_one=$((sum_one / count))
+                      avg_two=$((sum_two / count))
+                    fi
+                    echo "Averaged over $count runs:"
+                    echo "  parse:  $avg_parse"
+                    echo "  one:    $avg_one"
+                    echo "  two:    $avg_two"
+                    ;;
+                  *)
+                    wrapper "$@"
+                    ;;
+                esac
+              '';
+            })
+          ];
+        };
+    };
 }
