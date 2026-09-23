01f6db0ca51a8d43364c23e955be38a8b3d4f256
Michael Strobel <mstrobel97@gmail.com>
2026-09-23 09:54:49 +0200

    feat(base): add program helper

---
+++ lua/advent-of-code/helpers/program.lua
@@ -0,0 +1,84 @@
+--- @alias ProgramOperations table<string, fun(self: Program, table)>
+--- @alias ProgramHooks { pre?: fun(self: Program), post?: fun(self: Program) }
+--- @alias ProgramDebug fun(self: Program): boolean
+
+--- @class Program
+--- @field pointer integer
+--- @field registers table
+--- @field operations ProgramOperations
+--- @field hooks ProgramHooks
+--- @field debug ProgramDebug
+--- @field run fun(self: Program, instructions: table[]): table
+--- @field new fun(self: Program, registers: table, operations: ProgramOperations, hooks: ProgramHooks): Program
+local Program = {}
+
+Program = {
+  pointer = 1,
+  registers = {},
+  operations = {},
+  hooks = {},
+  debug = function()
+    return false
+  end,
+  --- @param self Program
+  --- @param registers table
+  --- @param operations ProgramOperations
+  --- @param hooks ProgramHooks
+  --- @param debug ProgramDebug
+  --- @return Program
+  new = function(self, registers, operations, hooks, debug)
+    return setmetatable({
+      pointer = 1,
+      registers = registers,
+      operations = operations,
+      hooks = hooks,
+      debug = debug,
+    }, { __index = self })
+  end,
+  --- @param self Program
+  --- @param instructions table[]
+  --- @return table
+  run = function(self, instructions)
+    if self.debug and self.debug(self) then
+      print(self.pointer, table.concat(self.registers, ", "))
+    end
+
+    while true do
+      local instruction = instructions[self.pointer]
+
+      if not instruction then
+        break
+      end
+
+      local operation = self.operations[instruction[1]]
+
+      if not operation then
+        break
+      end
+
+      if self.hooks.pre then
+        self.hooks.pre(self)
+      end
+
+      operation(self, instruction)
+
+      if self.hooks.post then
+        self.hooks.post(self)
+      end
+
+      self.pointer = self.pointer + 1
+
+      if self.debug and self.debug(self) then
+        print(self.pointer, table.concat(self.registers, ", "))
+      end
+    end
+
+    return self.registers
+  end,
+}
+
+--- @param parameters { registers: table, operations: ProgramOperations, hooks: ProgramHooks, debug: ProgramDebug, instructions: table[] }
+function _G.Program(parameters)
+  return Program:new(parameters.registers, parameters.operations, parameters.hooks, parameters.debug)
+    :run(parameters.instructions)
+end
