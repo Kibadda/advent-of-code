dafc65042156871c5faafee54bcb3971acdf1c40
Michael Strobel <mstrobel97@gmail.com>
2026-09-26 00:53:47 +0200

    feat(2019): add intcoder class

---
+++ lua/advent-of-code/2019/intcoder.lua
@@ -0,0 +1,94 @@
+--- @alias IntcoderOpcodes { parameters: integer, func: fun(self: Intcoder, parameters: integer[], modes: 0|1[]): boolean? }
+
+--- @class Intcoder
+--- @field pointer integer
+--- @field program integer[]
+--- @field opcodes table<integer, IntcoderOpcodes>
+--- @field output table
+--- @field new fun(program: string): Intcoder
+--- @field get fun(self: Intcoder, parameter: integer, mode: 0|1): integer
+--- @field active fun(self: Intcoder, opcodes: integer[]): Intcoder
+--- @field setup fun(self: Intcoder, func: fun(intcoder: Intcoder)): Intcoder
+--- @field run fun(self: Intcoder): Intcoder
+local Intcoder = {}
+
+--- @type table<integer, IntcoderOpcodes>
+local OPCODES = {
+  [1] = {
+    parameters = 3,
+    func = function(self, parameters, modes)
+      self.program[parameters[3] + 1] = self:get(parameters[1], modes[1]) + self:get(parameters[2], modes[2])
+    end,
+  },
+  [2] = {
+    parameters = 3,
+    func = function(self, parameters, modes)
+      self.program[parameters[3] + 1] = self:get(parameters[1], modes[1]) * self:get(parameters[2], modes[2])
+    end,
+  },
+}
+
+--- @type Intcoder
+Intcoder = {
+  pointer = 1,
+  program = {},
+  output = {},
+  opcodes = {},
+  new = function(program)
+    return setmetatable({
+      pointer = 1,
+      program = program:only_ints "-?%d+",
+      opcodes = table.deepcopy(OPCODES),
+      output = {},
+    }, { __index = Intcoder })
+  end,
+  get = function(self, parameter, mode)
+    return mode == 0 and self.program[parameter + 1] or parameter
+  end,
+  active = function(self, opcodes)
+    for _, opcode in ipairs(table.keys(self.opcodes)) do
+      if not table.contains(opcodes, opcode) then
+        self.opcodes[opcode] = nil
+      end
+    end
+
+    return self
+  end,
+  setup = function(self, func)
+    func(self)
+
+    return self
+  end,
+  run = function(self)
+    while true do
+      local opcode = self.program[self.pointer]
+      local modes = {
+        math.floor(opcode / 100) % 10,
+        math.floor(opcode / 1000) % 10,
+        math.floor(opcode / 10000) % 10,
+      }
+      opcode = opcode % 100
+
+      if not opcode or opcode == 99 or not self.opcodes[opcode] then
+        break
+      end
+
+      local opc = self.opcodes[opcode]
+      local parameters = {}
+      for i = 1, opc.parameters do
+        table.insert(parameters, self.program[self.pointer + i])
+      end
+
+      if not opc.func(self, parameters, modes) then
+        self.pointer = self.pointer + opc.parameters + 1
+      end
+    end
+
+    return self
+  end,
+}
+
+--- @param program string
+function _G.Intcoder(program)
+  return Intcoder.new(program)
+end
